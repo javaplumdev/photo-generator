773746bd79fbadb9243268a8e1514388a14d99a6
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2023-01-04 21:23:26 +0800

    update

---
+++ photo-generator/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
+function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
